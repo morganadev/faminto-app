5e0209aae8185dca9f7776acd1fdcf03bc23b299
morganadev <morganasouza699@gmail.com>
2018-12-05 19:49:46 -0200

    Initial commit

---
+++ src/main/java/server/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Hello World");
+		
+		return "/home/index";
+	}
+}
