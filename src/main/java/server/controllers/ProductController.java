8eec1ab32939ab6cae5492c6f26669a1e0be913d
morganadev <morganasouza699@gmail.com>
2018-12-07 21:55:50 -0200

    Implementação de tabela e configuração da ProductDAO

--- src/main/java/server/controllers/ProductController.java
+++ src/main/java/server/controllers/ProductController.java
@@ -1,16 +1,22 @@
 package server.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import server.daos.ProductDAO;
+
 @RequestMapping("/produtos")
 @Controller
 public class ProductController {
+	@Autowired
+	private ProductDAO productDAO;
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView list() {
 		ModelAndView modelAndView = new ModelAndView("product/list");
+		modelAndView.addObject("produtos",productDAO.findAll());
 		return modelAndView;
 	}
 }
