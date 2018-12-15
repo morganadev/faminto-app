3f3d45ce852770ddcfc99d36a6fa6e43950822e1
morganadev <morganasouza699@gmail.com>
2018-12-14 22:26:41 -0200

    Implementação regra de negocio carrinho

---
+++ src/main/java/server/models/Carrinho.java
@@ -0,0 +1,26 @@
+package server.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Component
+@Scope(value=WebApplicationContext.SCOPE_SESSION)
+public class Carrinho {
+	List<Product> products = new ArrayList<>();
+	
+	public void ADD(Product product) {
+		products.add(product);
+		System.out.println("Adicionando o produto " + product.getNome());
+		System.out.println("O carrinho tem " + products.size() + " produto(s)");
+	}
+
+	public List<Product> get() {
+		return products;
+		
+	}
+	
+}
